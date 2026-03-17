be43807d6f283edf1adeb0132e5ae5d22656a1c7
Pavel <your@email.com>
2026-03-17 21:16:42 +0700

    добавлен заголовок списка и кнопка добавления города в список

--- lib/features/view/tracked_cities_screen.dart
+++ lib/features/view/tracked_cities_screen.dart
@@ -18,6 +18,45 @@ class TrackedCitiesScreen extends StatelessWidget {
               ),
             ),
           ),
+
+          // содержимое
+          Column(
+            children: [
+              SizedBox(height: 70),
+
+              // заголовок списка + кнопка добавления города в список
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Text(
+                      "Отслеживаемые города",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    Spacer(),
+
+                    // кнопка
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, "/addMainCity");
+                      },
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.white,
+                        size: 28,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+            ],
+          )
         ],
       ),
     );
