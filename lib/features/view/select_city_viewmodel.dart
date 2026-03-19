e19a9ffc6cf28a7a69c3ad48ec6d64db96a3c4d0
paveldarensky <pashok.darenskii@gmail.com>
2026-03-19 18:22:14 +0700

    обработка загрузки результатов поиска по городам

--- lib/features/view/select_city_viewmodel.dart
+++ lib/features/view/select_city_viewmodel.dart
@@ -1,4 +1,8 @@
+import 'package:air_check/app/app_viewmodel.dart';
+import 'package:air_check/models/city.dart';
+import 'package:air_check/services/city_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SelectCityView extends StatefulWidget {
   final bool isSelectingForTracking; // флаг дял переключения выбора городов между 'основным' и 'добавлением дополнительного для отслеживания'
@@ -13,10 +17,14 @@ class SelectCityView extends StatefulWidget {
 }
 
 class _SelectCityViewState extends State<SelectCityView> {
+  final cityService = CityService();
 
+  List<String> cities = [];
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
+    final vm = Provider.of<AppViewModel>(context, listen: false);
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -56,11 +64,83 @@ class _SelectCityViewState extends State<SelectCityView> {
 
               Expanded(
                 child: Container(
-                  margin: EdgeInsets.fromLTRB(16, 0, 16, 80),
+                  margin: EdgeInsets.symmetric(horizontal: 16),
                   padding: EdgeInsets.symmetric(vertical: 8),
                   decoration: BoxDecoration(
                     color: Color.fromRGBO(0, 0, 0, 0.23),
                     borderRadius: BorderRadius.circular(25),
+                  ),
+
+                  child: isLoading
+                      ? Center(child: CircularProgressIndicator())
+                      : ListView.builder(
+                    itemCount: cities.length,
+                    itemBuilder: (context, index) {
+                      final city = cities[index];
+
+                      return GestureDetector(
+                        onTap: () async {
+                          if (widget.isSelectingForTracking) {
+                            final success =
+                            await vm.addTrackedCityByName(city);
+
+                            if (!success) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text(
+                                      'Город уже добавлен или не найден'),
+                                ),
+                              );
+                              return;
+                            }
+
+                            Navigator.pop(context);
+                          } else {
+                            final coords =
+                            await cityService.getCoordByName(city);
+
+                            if (coords == null) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                    content:
+                                    Text('Город не найден')),
+                              );
+                              return;
+                            }
+
+                            final aqd = await vm.airRepository
+                                .loadAirQualityByCoord(coords);
+
+                            final newCity = City(city, aqd, coords);
+
+                            vm.setMainCity(newCity);
+                            await vm.saveMainCity();
+
+                            Navigator.pushReplacementNamed(
+                                context, "/home");
+                          }
+                        },
+                        child: Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 3.5),
+                          padding: EdgeInsets.symmetric(
+                              vertical: 15, horizontal: 16),
+                          decoration: BoxDecoration(
+                            color:
+                            Color.fromRGBO(255, 255, 255, 0.2),
+                            borderRadius:
+                            BorderRadius.circular(10),
+                          ),
+                          child: Text(
+                            city,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   ),
                 ),
               ),
