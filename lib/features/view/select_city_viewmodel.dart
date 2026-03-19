91ec4eac1471535fe8e84d57648988aee8873434
Yunhich <berezovskii05@mail.ru>
2026-03-19 23:38:11 +0700

    Update select_city_viewmodel.dart

--- lib/features/view/select_city_viewmodel.dart
+++ lib/features/view/select_city_viewmodel.dart
@@ -1,24 +1,9 @@
-import 'package:air_check/app/app_viewmodel.dart';
-import 'package:air_check/models/city.dart';
-import 'package:air_check/services/city_service.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:air_check/services/city_service.dart';
 
-class SelectCityView extends StatefulWidget {
-  final bool isSelectingForTracking; // флаг дял переключения выбора городов между 'основным' и 'добавлением дополнительного для отслеживания'
-
-  const SelectCityView({
-    super.key,
-    this.isSelectingForTracking = false,
-  });
-
-  @override
-  State<SelectCityView> createState() => _SelectCityViewState();
-}
-
-class _SelectCityViewState extends State<SelectCityView> {
-  final cityService = CityService();
+class SelectCityViewModel extends ChangeNotifier {
+  final CityService cityService = CityService();
 
   List<String> cities = [];
   bool isLoading = false;
@@ -29,159 +14,26 @@ class _SelectCityViewState extends State<SelectCityView> {
     if (_debounce?.isActive ?? false) _debounce!.cancel();
 
     _debounce = Timer(const Duration(milliseconds: 500), () async {
-      if (query.isEmpty) return;
+      if (query.isEmpty) {
+        cities = [];
+        notifyListeners();
+        return;
+      }
 
-      setState(() => isLoading = true);
+      isLoading = true;
+      notifyListeners();
 
       final result = await cityService.searchCities(query);
 
-      setState(() {
-        cities = result;
-        isLoading = false;
-      });
+      cities = result;
+      isLoading = false;
+      notifyListeners();
     });
   }
 
-  @override
+  @id86240433 (@override)
   void dispose() {
     _debounce?.cancel();
     super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final vm = Provider.of<AppViewModel>(context, listen: false);
-
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/images/onboarding_background.jpeg"),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Column(
-            children: [
-              SizedBox(height: 70),
-
-              // поиск
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 16),
-                child: TextField(
-                  onChanged: search,
-                  decoration: InputDecoration(
-                    hintText: "Введите название города",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    fillColor: Colors.white,
-                    suffixIcon: Icon(Icons.search, color: Colors.black),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 40),
-
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(16, 0, 16, 80),
-                  padding: EdgeInsets.symmetric(vertical: 8),
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(0, 0, 0, 0.23),
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-
-                  child: isLoading
-                      ? Center(child: CircularProgressIndicator())
-                      : cities.isEmpty
-                      ? Center(
-                    child: Text(
-                      "Начните вводить название города",
-                      style: TextStyle(
-                        color: Colors.white70,
-                        fontSize: 16,
-                      ),
-                    ),
-                  )
-                      : ListView.builder(
-                    itemCount: cities.length,
-                    itemBuilder: (context, index) {
-                      final city = cities[index];
-
-                      return GestureDetector(
-                        onTap: () async {
-                          if (widget.isSelectingForTracking) {
-                            final success =
-                            await vm.addTrackedCityByName(city);
-
-                            if (!success) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text(
-                                      'Город уже добавлен или не найден'),
-                                ),
-                              );
-                              return;
-                            }
-
-                            Navigator.pop(context);
-                          } else {
-                            final coords =
-                            await cityService.getCoordByName(city);
-
-                            if (coords == null) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                    content:
-                                    Text('Город не найден')),
-                              );
-                              return;
-                            }
-
-                            final aqd = await vm.airRepository
-                                .loadAirQualityByCoord(coords);
-
-                            final newCity = City(city, aqd, coords);
-
-                            vm.setMainCity(newCity);
-                            await vm.saveMainCity();
-
-                            Navigator.pushReplacementNamed(
-                                context, "/home");
-                          }
-                        },
-                        child: Container(
-                          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-                          padding: EdgeInsets.symmetric(vertical: 14, horizontal: 16),
-                          decoration: BoxDecoration(
-                            color:
-                            Color.fromRGBO(255, 255, 255, 0.2),
-                            borderRadius:
-                            BorderRadius.circular(10),
-                          ),
-                          child: Text(
-                            city,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
   }
 }
